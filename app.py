704203b41dd4716a50e7c71b7f22c73069d113f6
SongZihan <zihan.song@freemud.com>
2019-04-11 13:41:40 +0800

    sort search result

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sys
+import functools
 
 sys.path.append("..")
 sys.path.append("./mdx_resolve")
@@ -9,6 +10,15 @@ from flask_restful import reqparse, abort, Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+
+def cmp_ignore_case(s1, s2):
+    s1 = s1.lower()
+    s2 = s2.lower()
+    if s1 < s2:
+        return -1
+    elif s1 > s2:
+        return 1
+    return 0
 
 # 搜索匹配的单词，返回10个
 class SearchWord(Resource):
@@ -21,7 +31,7 @@ class SearchWord(Resource):
         search = args['search']
         print(search)
         builder = IndexBuilder('./mdx_resolve/mdx/Collins.mdx')
-        result = builder.get_mdx_keys("*" + search + "*")[:10]
+        result = sorted(builder.get_mdx_keys(search + "*")[:20], key=functools.cmp_to_key(cmp_ignore_case))
         # todo 优化搜索结果 使其与搜索字符串最匹配
 
         result_json = [{"name": word_name} for word_name in result]
@@ -31,4 +41,5 @@ class SearchWord(Resource):
 api.add_resource(SearchWord, '/searchWord', methods=['POST'])
 
 if __name__ == '__main__':
+    # app.run(host='0.0.0.0', port=5000)
     app.run(debug=True)
